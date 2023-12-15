b3f4d4cee72382b2a311f8f11e215386fdf66898
Grootoportunities <tapir1337@mail.ru>
2023-12-15 16:20:07 +0300

    03 input

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
 import "./App.css";
 import { InsidesPropsType, Todolist } from "./Todolist";
+import { v1 } from "uuid";
 
 export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
   let [movies, setMovies] = useState<InsidesPropsType[]>([
-    { id: 1, name: "Banshee Inisherin", checked: true },
-    { id: 2, name: "Kid of the human", checked: true },
-    { id: 3, name: "Memento", checked: true },
-    { id: 4, name: "Whale", checked: true },
-    { id: 5, name: "Twelve friends of Oushn", checked: false },
-    { id: 6, name: "Seventh mile", checked: false },
+    { id: v1(), name: "Banshee Inisherin", checked: true },
+    { id: v1(), name: "Kid of the human", checked: true },
+    { id: v1(), name: "Memento", checked: true },
+    { id: v1(), name: "Whale", checked: true },
+    { id: v1(), name: "Twelve friends of Oushn", checked: false },
+    { id: v1(), name: "Seventh mile", checked: false },
   ]);
   let [filter, setFilter] = useState<FilterValuesType>("all");
 
-  function deleteMovies(id: number) {
+  function deleteMovies(id: string) {
     let filteredMovies = movies.filter((item) => item.id !== id);
     setMovies(filteredMovies);
+  }
+
+  function addMovie(movieName: string) {
+    let newMovie = { id: v1(), name: movieName, checked: false };
+    let newMovies = [newMovie, ...movies];
+    setMovies(newMovies);
   }
 
   function changeFilter(value: FilterValuesType) {
@@ -39,6 +46,7 @@ function App() {
         insides={moviesForTodolist}
         deleteMovies={deleteMovies}
         changeFilter={changeFilter}
+        addMovie={addMovie}
       />
     </div>
   );
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,56 +1,70 @@
-import React from "react";
-import {FilterValuesType} from "./App";
-
+import React, { ChangeEvent, KeyboardEvent, useState } from "react";
+import { FilterValuesType } from "./App";
 
 export type InsidesPropsType = {
-    id: number
-    name: string
-    checked: boolean
-}
+  id: string;
+  name: string;
+  checked: boolean;
+};
 
 type TodolistPropsType = {
-    hat: string
-    insides: InsidesPropsType[]
-    deleteMovies: (id: number) => void
-    changeFilter: (value: FilterValuesType) => void
-}
+  hat: string;
+  insides: InsidesPropsType[];
+  deleteMovies: (id: string) => void;
+  changeFilter: (value: FilterValuesType) => void;
+  addMovie: (movieName: string) => void;
+};
 
 export function Todolist(props: TodolistPropsType) {
+  const [newMovieName, setNewMovieName] = useState("");
 
-    const mappedInsides = props.insides.map(item =>
-        <li><input type="checkbox" checked={item.checked}/>
-            <span>{item.name}</span>
-            <button onClick={() => {
-                props.deleteMovies(item.id)
-            }}>X
-            </button>
-        </li>
-    )
+  const mappedInsides = props.insides.map((item) => {
+    const onDeleteHandler = () => {
+      props.deleteMovies(item.id);
+    };
 
     return (
-        <div>
-            <h3>{props.hat}</h3>
-            <div>
-                <input/>
-                <button>+</button>
-            </div>
-            <ul>
-                {mappedInsides}
-            </ul>
-            <div>
-                <button onClick={() => {
-                    props.changeFilter("all")
-                }}>All
-                </button>
-                <button onClick={() => {
-                    props.changeFilter("active")
-                }}>Active
-                </button>
-                <button onClick={() => {
-                    props.changeFilter("completed")
-                }}>Completed
-                </button>
-            </div>
-        </div>
-    )
+      <li key={item.id}>
+        <input type="checkbox" checked={item.checked} />
+        <span>{item.name}</span>
+        <button onClick={onDeleteHandler}>X</button>
+      </li>
+    );
+  });
+  const onChangeNewMovieNameHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewMovieName(e.currentTarget.value);
+  };
+  const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.charCode === 13) {
+      props.addMovie(newMovieName);
+      setNewMovieName("");
+    }
+  };
+  const addMovie = () => {
+    props.addMovie(newMovieName);
+    setNewMovieName("");
+  };
+  const onAllClickHander = () => props.changeFilter("all");
+  const onActiveClickHander = () => props.changeFilter("active");
+  const onCompletedClickHander = () => props.changeFilter("completed");
+
+  return (
+    <div>
+      <h3>{props.hat}</h3>
+      <div>
+        <input
+          value={newMovieName}
+          onChange={onChangeNewMovieNameHandler}
+          onKeyPress={onKeyPressHandler}
+        />
+        <button onClick={addMovie}>+</button>
+      </div>
+      <ul>{mappedInsides}</ul>
+      <div>
+        <button onClick={onAllClickHander}>All</button>
+        <button onClick={onActiveClickHander}>Active</button>
+        <button onClick={onCompletedClickHander}>Completed</button>
+      </div>
+    </div>
+  );
 }
