5221102efc945b4a5914cedd6d6486451663e851
Grootoportunities <tapir1337@mail.ru>
2024-06-24 14:22:51 +0300

    slice error

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1/",
   withCredentials: true,
-  headers: { "API-KEY": "e495278a-c2d8-4576-bed3-b59cbb54bf9d" },
+  headers: { "API-KEY": "a918545c-774f-48b4-a615-795c2cc7eda0" },
 });
